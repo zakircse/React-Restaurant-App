bb8035ea6f0278aa71aea3233af4f52a9e64af29
Md. Zakir Hossain <zakir.rars@gmail.com>
2022-04-14 20:24:12 +0600

    Designed dish details and load comments

---
+++ src/components/body/DishDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
+import LoadComments from "./LoadComments";
+
+const DishDetail = (props) => {
+    return (
+        <div>
+            <Card style={{ marginTop: "10px" }}>
+                <CardImg top src={props.dish.image} alt={props.dish.name} />
+                <CardBody style={{ textAlign: "left" }}>
+                    <CardTitle>{props.dish.name}</CardTitle>
+                    <CardText>
+                        {props.dish.description}
+                    </CardText>
+                    <CardText>
+                        Price: {props.dish.price}/-
+                    </CardText>
+                    <hr />
+                    <LoadComments comments={props.dish.comments} />
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+export default DishDetail;
