2083ee74dd9338004294272421234e26a62475c2
Md. Zakir Hossain <zakir.rars@gmail.com>
2022-04-14 13:07:24 +0600

    Navigation Bar using Reactstrap

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 const Header = () => {
     return (
-        <div></div>
+        <div>
+            <Navigation />
+        </div>
     );
 }
 export default Header;
